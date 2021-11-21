f682045dd25b2da74914aa6d362f8510e81c0807
Aditi <aditis.0520@gmail.com>
2021-11-21 10:59:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('scroll-up-btn').addClass("show");
+        }
+        else{
+            $('scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    // typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Technophile...","Web Developer...", "Believer...","Pluviophile...","Learner...",  "Optimist...", ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Technophile...","Web Developer...", "Believer...", "Learner...",  "Optimist...", ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+//toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggle("active");
+    $('.menu-btn i').toggle("active");
+});
+});
+ 
+
+
+
